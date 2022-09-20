fbae2befb1e38b629b061f1cb4c55dc2688c3f21
jmacura <macurajan@gmail.com>
2022-09-21 00:13:08 +0200

    💄 make output friendly

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import data from './assets/elections.json'
   import type {Election, Referendum} from './lib/elections.type'
-  import Block from './lib/Block.svelte';
+  import EventBlock from './lib/EventBlock.svelte';
 
   const sortByStartDate = (a: Election | Referendum, b: Election | Referendum) => {
     // When exact startDate is not available, compare just by year
@@ -13,23 +13,31 @@
   console.log(data)
   const electionData = [...data.main, ...data.auxillary, ...data.referendums].sort(sortByStartDate)
   console.log(electionData)
+
+  //TODO: We have to work with the endDate here, NOT counting the +1 day manually
+  const now = new Date('2022-09-24').getTime()
+  const timeToNearestEvent = Math.min(...electionData.map((event) => {
+    const timeToStart = new Date(event.startDate).getTime() - now
+    return timeToStart >= -24* 60 * 60 * 1000 ? timeToStart : Infinity
+  }))
+  console.log(timeToNearestEvent)
+  // When multiple elections happen on the same date, they are all nearest
+  const nearestEvents = electionData.filter((el) => {
+    return new Date(el.startDate).getTime() - now == timeToNearestEvent
+  })
+  console.log(nearestEvents)
 </script>
 
+<div class="fixed top-0 left-0 w-screen bg-white bg-opacity-50"><h1 class="mx-auto my-2">Kdy budou volby?</h1></div>
 <main>
-  <h1>Kdy budou volby?</h1>
 
-  {#each electionData as election}
-  <div class="container flex">
-    <Block election={election} />
+  <div class="container flex flex-col mt-10">
+    {#each electionData as election}
+      <EventBlock election="{election}" firstInLine="{nearestEvents.includes(election)}" />
+    {/each}
   </div>
-  {/each}
 
 </main>
 
 <style>
-@tailwind base;
-
-@tailwind components;
-
-@tailwind utilities;
 </style>
---
+++ src/lib/EventBlock.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+  import fullNames from "../assets/fullNamesMap.json"
+  import type { Election, Referendum } from "./elections.type"
+
+  export let election: Election | Referendum
+  export let firstInLine: boolean
+
+  const currentYear = new Date().getFullYear()
+  const now = new Date().getTime()
+  /**
+   * Yes, this code is kind of reinventing the wheel, but the svelte-time lib didn't work...
+   * @param date
+   */
+  const formatDate = (date) => {
+    const monthWords = ['ledna', 'února', 'března', 'dubna', 'května', 'června', 'července', 'srpna', 'září', 'října', 'listopadu', 'prosince']
+    const eventDate = new Date(date)
+    const d = eventDate.getDate()
+    const m = monthWords[eventDate.getMonth()]
+    const y = eventDate.getFullYear()
+    const daysToEvent = (eventDate.getTime() - now) / (1000 * 60 * 60 * 24)
+    if (daysToEvent < 0) {
+      return `dnes`
+    }
+    if (daysToEvent < 1) {
+      return `zítra`
+    }
+    if (daysToEvent < 4) {
+      return `za ${Math.ceil(daysToEvent)} dny`
+    }
+    if (currentYear < y) {
+      return `${d}. ${m} ${y}`
+    }
+    return `${d}. ${m}`
+  }
+</script>
+
+<div class="m-2" class:hidden="{election.year < currentYear}" class:my-6="{firstInLine}">
+<div class="text-2xl" class:text-7xl="{firstInLine}" class:font-bold="{firstInLine}">
+  {formatDate(election.startDate ?? election.year)}
+</div>
+<div class="text-gray-700 m-1" class:text-2xl="{firstInLine}">
+  {election.type == 'referendum' ? election.name : fullNames[election.type]}{election.round ? `, ${election.round}. kolo` : ''}
+</div>
+</div>
+
+<style></style>
