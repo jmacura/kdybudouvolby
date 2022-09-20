1e8b913a103918649a0d72497f4cd661350b0dcf
jmacura <macurajan@gmail.com>
2022-09-20 17:54:55 +0200

    ✨ show the content

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,35 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  import data from './assets/elections.json'
+  import type {Election, Referendum} from './lib/elections.type'
+  import Block from './lib/Block.svelte';
+
+  const sortByStartDate = (a: Election | Referendum, b: Election | Referendum) => {
+    // When exact startDate is not available, compare just by year
+    if (!a.startDate || !b.startDate) {
+      return a.year > b.year ? 1 : -1
+    }
+    return new Date(a.startDate).getTime() > new Date(b.startDate).getTime() ? 1 : -1
+  }
+  console.log(data)
+  const electionData = [...data.main, ...data.auxillary, ...data.referendums].sort(sortByStartDate)
+  console.log(electionData)
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+  <h1>Kdy budou volby?</h1>
+
+  {#each electionData as election}
+  <div class="container flex">
+    <Block election={election} />
   </div>
-  <h1>Vite + Svelte</h1>
+  {/each}
 
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+@tailwind base;
+
+@tailwind components;
+
+@tailwind utilities;
 </style>
