00c6e0756de16c9bfd82fc58e293e77ab1687d21
jmacura <macurajan@gmail.com>
2022-09-24 23:39:54 +0200

    ✨ add elections until 2050

--- src/lib/EventBlock.svelte
+++ src/lib/EventBlock.svelte
@@ -11,12 +11,15 @@
    * Yes, this code is kind of reinventing the wheel, but the svelte-time lib didn't work...
    * @param date
    */
-  const formatDate = (date) => {
+  const formatDate = (date: string) => {
+    if (date.length <= 4) {
+      return date
+    }
     const monthWords = ['ledna', 'února', 'března', 'dubna', 'května', 'června', 'července', 'srpna', 'září', 'října', 'listopadu', 'prosince']
     const eventDate = new Date(date)
-    const d = eventDate.getDate()
-    const m = monthWords[eventDate.getMonth()]
     const y = eventDate.getFullYear()
+    const m = monthWords[eventDate.getMonth()]
+    const d = eventDate.getDate()
     const daysToEvent = (eventDate.getTime() - now) / (1000 * 60 * 60 * 24)
     if (daysToEvent < 0) {
       return `dnes`
@@ -27,10 +30,10 @@
     if (daysToEvent < 4) {
       return `za ${Math.ceil(daysToEvent)} dny`
     }
-    if (currentYear < y) {
-      return `${d}. ${m} ${y}`
+    if (currentYear == y) {
+      return `${d}. ${m}`
     }
-    return `${d}. ${m}`
+    return `${d}. ${m} ${y}`
   }
 
   const isPastElection = () => {
@@ -44,7 +47,7 @@
 
 <div class="m-2" class:hidden="{isPastElection()}" class:my-6="{firstInLine}">
 <div class="text-2xl" class:text-7xl="{firstInLine}" class:font-bold="{firstInLine}">
-  {formatDate(election.startDate ?? election.year)}
+  {formatDate(election.startDate ?? `${election.year}`)}
 </div>
 <div class="text-gray-700 m-1" class:text-2xl="{firstInLine}">
   {election.type == 'referendum' ? election.name : fullNames[election.type]}{election.round ? `, ${election.round}. kolo` : ''}
