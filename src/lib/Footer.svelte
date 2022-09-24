29b9bb87c9851b634263248502c133903f1f38a6
jmacura <macurajan@gmail.com>
2022-09-25 00:14:20 +0200

    💬 reword footer

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,3 +1,3 @@
 <div class="text-gray-500 text-sm mt-20">
-  Data from <a href="https://volby.cz/">volby.cz</a>. Source code at <a href="https://github.com/jmacura/kdybudouvolby">GitHub</a>.
+  Aktuální a historická data z <a href="https://volby.cz/">volby.cz</a>. Zdrojový kód na <a href="https://github.com/jmacura/kdybudouvolby">GitHubu</a>.
 </div>
